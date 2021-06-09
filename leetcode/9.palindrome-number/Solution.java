ba7ac824e0ad134c0baaa2540b2a925f51667148
Julio Gutierrez <bubuntux@gmail.com>
2021-06-09 18:57:57 -0500

    9. Palindrome Number

---
+++ leetcode/9.palindrome-number/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reverse = 0;
+        int tmp = x;
+        while (tmp > 0) {
+            reverse = reverse * 10 + tmp % 10;
+            tmp /= 10;
+        }
+        return reverse == x;
+    }
+}
