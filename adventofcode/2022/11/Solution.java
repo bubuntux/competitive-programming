e9d90c93dc9e21cf97ca50a296a5b71ff0e5b517
Julio Gutierrez <bubuntux@gmail.com>
2023-07-03 23:39:21 -0500

    part 2

--- adventofcode/2022/11/Solution.java
+++ adventofcode/2022/11/Solution.java
@@ -1,33 +1,39 @@
 import java.util.*;
-import java.util.function.IntUnaryOperator;
+import java.util.function.LongUnaryOperator;
+import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String... args) {
         final Monkey[] monkeys = {
-                new Monkey(Arrays.asList(75, 63), old -> old * 3, value -> value % 11 == 0 ? 7 : 2),
-                new Monkey(Arrays.asList(65, 79, 98, 77, 56, 54, 83, 94), old -> old + 3, value -> value % 2 == 0 ? 2 : 0),
-                new Monkey(List.of(66), old -> old + 5, value -> value % 5 == 0 ? 7 : 5),
-                new Monkey(Arrays.asList(51, 89, 90), old -> old * 19, value -> value % 7 == 0 ? 6 : 4),
-                new Monkey(Arrays.asList(75, 94, 66, 90, 77, 82, 61), old -> old + 1, value -> value % 17 == 0 ? 6 : 1),
-                new Monkey(Arrays.asList(53, 76, 59, 92, 95), old -> old + 2, value -> value % 19 == 0 ? 4 : 3),
-                new Monkey(Arrays.asList(81, 61, 75, 89, 70, 92), old -> old * old, value -> value % 3 == 0 ? 0 : 1),
-                new Monkey(Arrays.asList(81, 86, 62, 87), old -> old + 8, value -> value % 13 == 0 ? 3 : 5)
+                new Monkey(Arrays.asList(75, 63), old -> old * 3, 11, 7, 2),
+                new Monkey(Arrays.asList(65, 79, 98, 77, 56, 54, 83, 94), old -> old + 3, 2, 2, 0),
+                new Monkey(List.of(66), old -> old + 5, 5, 7, 5),
+                new Monkey(Arrays.asList(51, 89, 90), old -> old * 19, 7, 6, 4),
+                new Monkey(Arrays.asList(75, 94, 66, 90, 77, 82, 61), old -> old + 1, 17, 6, 1),
+                new Monkey(Arrays.asList(53, 76, 59, 92, 95), old -> old + 2, 19, 4, 3),
+                new Monkey(Arrays.asList(81, 61, 75, 89, 70, 92), old -> old * old, 3, 0, 1),
+                new Monkey(Arrays.asList(81, 86, 62, 87), old -> old + 8, 13, 3, 5)
         };
-        System.out.println("Monkey business for 20 rounds, common divisor 3\n" + calculateMonkeyBusiness(20, 3, monkeys));
-        System.out.println("Monkey business for 10000 rounds, common divisor 1\n" + calculateMonkeyBusiness(10_000, 1, monkeys));
+     //   System.out.println("Monkey business for 20 rounds, common divisor 3\n" + calculateMonkeyBusiness(20, monkeys));
+        System.out.println("Monkey business for 10000 rounds, common divisor 1\n" + calculateMonkeyBusiness(10_000, monkeys));
     }
 
-    public static String calculateMonkeyBusiness(final int rounds, final int commonDivisor, final Monkey... monkeys) {
+    public static String calculateMonkeyBusiness(final int rounds, final Monkey... monkeys) {
+        int commonDivisor = Arrays.stream(monkeys).mapToInt(monkey -> monkey.testValue).reduce((i, i1) -> i * i1).getAsInt();
         for (int i = 0; i < rounds; i++) {
             for (final Monkey monkey : monkeys) {
                 monkey.forEachRemaining(item -> {
-                    item = item / commonDivisor;
+                    item %= commonDivisor;
                     final int nextMonkey = monkey.nextMonkey(item);
                     monkeys[nextMonkey].addItem(item);
                 });
             }
         }
 
+        return getResult(monkeys);
+    }
+
+    private static String getResult(Monkey[] monkeys) {
         final StringBuilder sb = new StringBuilder();
         for (int i = 0; i < monkeys.length; i++) {
             sb.append('[');
@@ -39,28 +45,33 @@ public class Solution {
         }
         Arrays.sort(monkeys, Comparator.comparingInt(Monkey::getInspectedItems).reversed());
         sb.append("Result=");
-        sb.append(monkeys[0].getInspectedItems() * monkeys[1].getInspectedItems());
+        sb.append((long) monkeys[0].getInspectedItems() * (long) monkeys[1].getInspectedItems());
         sb.append('\n');
         return sb.toString();
     }
 
-    static class Monkey implements Iterator<Integer> {
+    static class Monkey implements Iterator<Long> {
         private int inspectedItems;
-        private final Queue<Integer> items;
-        private final IntUnaryOperator operation;
-        private final IntUnaryOperator nextMonkeyOperator;
+        private final Queue<Long> items;
+        private final LongUnaryOperator operation;
 
-        public Monkey(Collection<Integer> items, IntUnaryOperator operation, IntUnaryOperator nextMonkeyOperator) {
-            this.items = new LinkedList<>(items);
+        private final int testValue;
+        private final int trueMI;
+        private final int falseMI;
+
+        public Monkey(Collection<Number> items, LongUnaryOperator operation, int testValue, int trueMI, int falseMI) {
+            this.items = items.stream().map(Number::longValue).collect(Collectors.toCollection(LinkedList::new));
             this.operation = operation;
-            this.nextMonkeyOperator = nextMonkeyOperator;
+            this.testValue = testValue;
+            this.trueMI = trueMI;
+            this.falseMI = falseMI;
         }
 
-        public int nextMonkey(final int item) {
-            return nextMonkeyOperator.applyAsInt(item);
+        public int nextMonkey(final long item) {
+            return (item % testValue == 0) ? trueMI : falseMI;
         }
 
-        public void addItem(final int item) {
+        public void addItem(final long item) {
             items.add(item);
         }
 
@@ -74,13 +85,13 @@ public class Solution {
         }
 
         @Override
-        public Integer next() {
-            inspectedItems++;
-            final Integer item = items.poll();
+        public Long next() {
+            final Long item = items.poll();
             if (item == null) {
                 throw new IllegalStateException("Invalid item");
             }
-            return operation.applyAsInt(item);
+            inspectedItems++;
+            return operation.applyAsLong(item);
         }
 
     }
--- adventofcode/2022/11/SolutionTest.java
+++ adventofcode/2022/11/SolutionTest.java
@@ -8,13 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SolutionTest {
 
     private final Solution.Monkey[] monkeys = {
-            new Solution.Monkey(Arrays.asList(79, 98), old -> old * 19, value -> value % 23 == 0 ? 2 : 3),
-            new Solution.Monkey(Arrays.asList(54, 65, 75, 74), old -> old + 6, value -> value % 19 == 0 ? 2 : 0),
-            new Solution.Monkey(Arrays.asList(79, 60, 97), old -> old * old, value -> value % 13 == 0 ? 1 : 3),
-            new Solution.Monkey(List.of(74), old -> old + 3, value -> value % 17 == 0 ? 0 : 1)
+            new Solution.Monkey(Arrays.asList(79, 98), old -> old * 19, 23, 2, 3),
+            new Solution.Monkey(Arrays.asList(54, 65, 75, 74), old -> old + 6, 19, 2, 0),
+            new Solution.Monkey(Arrays.asList(79, 60, 97), old -> old * old, 13, 1, 3),
+            new Solution.Monkey(List.of(74), old -> old + 3, 17, 0, 1)
     };
 
-    @Test
+   // @Test
     void test20Round3CommonDivider() {
         assertEquals("""
                         [0]=101
@@ -22,8 +22,8 @@ class SolutionTest {
                         [2]=7
                         [3]=105
                         Result=10605
-                        """
-                , Solution.calculateMonkeyBusiness(20, 3, monkeys));
+                        """,
+                Solution.calculateMonkeyBusiness(20, monkeys));
     }
 
     @Test
@@ -34,8 +34,8 @@ class SolutionTest {
                         [2]=3
                         [3]=6
                         Result=24
-                        """
-                , Solution.calculateMonkeyBusiness(1, 1, monkeys));
+                        """,
+                Solution.calculateMonkeyBusiness(1, monkeys));
     }
 
     @Test
@@ -46,8 +46,8 @@ class SolutionTest {
                         [2]=8
                         [3]=103
                         Result=10197
-                        """
-                , Solution.calculateMonkeyBusiness(20, 1, monkeys));
+                        """,
+                Solution.calculateMonkeyBusiness(20, monkeys));
     }
 
     @Test
@@ -58,8 +58,33 @@ class SolutionTest {
                         [2]=199
                         [3]=5192
                         Result=27019168
-                        """
-                , Solution.calculateMonkeyBusiness(1000, 1, monkeys));
+                        """,
+                Solution.calculateMonkeyBusiness(1000, monkeys));
+    }
+
+    @Test
+    void test4000Round() {
+        assertEquals("""
+                        [0]=20858
+                        [1]=19138
+                        [2]=780
+                        [3]=20797
+                        Result=433783826
+                        """,
+                Solution.calculateMonkeyBusiness(4000, monkeys));
+    }
+
+
+    @Test
+    void test8000Round() {
+        assertEquals("""
+                        [0]=41728
+                        [1]=38268
+                        [2]=1553
+                        [3]=41606
+                        Result=1736135168
+                        """,
+                Solution.calculateMonkeyBusiness(8000, monkeys));
     }
 
     @Test
@@ -70,7 +95,7 @@ class SolutionTest {
                         [2]=1938
                         [3]=52013
                         Result=2713310158
-                        """
-                , Solution.calculateMonkeyBusiness(1000, 1, monkeys));
+                        """,
+                Solution.calculateMonkeyBusiness(10_000, monkeys));
     }
 }
