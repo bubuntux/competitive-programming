4d9073dd354f25c18b814c31031288ea05ced1e0
Julio Gutierrez <bubuntux@gmail.com>
2023-09-08 22:14:32 -0500

    leetcode 8

---
+++ leetcode/8.string-to-integer-atoi/src/lib.rs
@@ -0,0 +1,64 @@
+//https://leetcode.com/problems/string-to-integer-atoi
+
+impl Solution {
+    pub fn my_atoi(s: String) -> i32 {
+        let s = s.trim_start();
+
+        let mut result = 0;
+        let mut sign = 1;
+        let mut chars = s.chars();
+
+        match chars.next() {
+            Some('+') => (),
+            Some('-') => sign = -1,
+            Some(c @ '0'..='9') => result = c.to_digit(10).unwrap() as i32,
+            _ => return result,
+        }
+
+        for c in chars {
+            match c {
+                '0'..='9' => {
+                    result = result
+                        .saturating_mul(10)
+                        .saturating_add(sign * c.to_digit(10).unwrap() as i32)
+                }
+                _ => break,
+            }
+        }
+
+        return result;
+    }
+}
+
+// #############
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let result = Solution::my_atoi(String::from("42"));
+        assert_eq!(result, 42);
+    }
+
+    #[test]
+    fn example_2() {
+        let result = Solution::my_atoi(String::from("     -42"));
+        assert_eq!(result, -42);
+    }
+
+    #[test]
+    fn example_3() {
+        let result = Solution::my_atoi(String::from("4193 with words"));
+        assert_eq!(result, 4193);
+    }
+
+    #[test]
+    fn example_4() {
+        let result = Solution::my_atoi(String::from("-91283472332"));
+        assert_eq!(result, -2147483648);
+    }
+}
