552776cebe13026410b5d3ccec65e0f9aa7f8a6e
Julio Gutierrez <test@test.com>
2024-12-02 18:43:25 -0600

    having issues with part 2

--- adventofcode/2024/src/day2/mod.rs
+++ adventofcode/2024/src/day2/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 fn get_reports(input: &str) -> Vec<Vec<isize>> {
     input
         .lines()
@@ -10,42 +12,51 @@ fn get_reports(input: &str) -> Vec<Vec<isize>> {
         .collect::<Vec<Vec<isize>>>()
 }
 
+fn is_safe(a: isize, b: isize, sign: isize) -> bool {
+    (1..=3).contains(&(b - a).mul(sign))
+}
+
 #[allow(dead_code)]
 fn part1(input: &str) -> usize {
     get_reports(input)
         .iter()
-        .fold(0, |mut acc, report| -> usize {
-            if is_safe(report) {
-                acc += 1
-            }
-            acc
-        })
-}
-
-fn is_safe(report: &[isize]) -> bool {
-    let mut direction: Option<bool> = None;
-    for (prev, curr) in report.iter().zip(report.iter().skip(1)) {
-        let mut curr_dir = false;
-        let diff = prev - curr;
-        if diff == 0 {
-            return false;
-        }
-        if diff > 0 {
-            curr_dir = true;
-        }
-        match direction {
-            None => direction = Some(curr_dir),
-            Some(prev_dir) => {
-                if prev_dir != curr_dir {
+        .filter(|report| {
+            let sign = (report[1] - report[0]).signum() | 1;
+            for (prev, curr) in report.iter().zip(report.iter().skip(1)) {
+                if !is_safe(*prev, *curr, sign) {
                     return false;
                 }
             }
-        }
+            true
+        })
+        .count()
+}
 
-        let abs_diff = prev.abs_diff(*curr);
-        if !(1..=3).contains(&abs_diff) {
+#[allow(dead_code)]
+fn part2(input: &str) -> usize {
+    get_reports(input)
+        .iter()
+        .filter(|report| is_report_safe(report, 1) || is_report_safe(report, -1))
+        .count()
+}
+
+fn is_report_safe(report: &[isize], sign: isize) -> bool {
+    let mut change = 0;
+    for w in report.windows(3) {
+        let safe1 = if change == 1 {
+            change = 2;
+            true
+        } else {
+            is_safe(w[0], w[1], sign)
+        };
+        let safe2 = is_safe(w[1], w[2], sign);
+        if safe1 && safe2 {
+            continue;
+        }
+        if change > 0 || !is_safe(w[0], w[2], sign) {
             return false;
         }
+        change = 1;
     }
     true
 }
@@ -56,18 +67,18 @@ mod test {
     use super::*;
     use std::fs;
 
-    #[test]
-    fn example1() {
-        let result = part1(
-            "
+    const EXAMPLE: &str = "
             7 6 4 2 1
             1 2 7 8 9
             9 7 6 2 1
             1 3 2 4 5
             8 6 4 4 1
             1 3 6 7 9
-            ",
-        );
+            ";
+
+    #[test]
+    fn example1() {
+        let result = part1(EXAMPLE);
         assert_eq!(result, 2);
     }
 
@@ -76,5 +87,22 @@ mod test {
         let input = fs::read_to_string("./src/day2/input").expect("read input");
         let result = part1(&input);
         print!("answer1 {}", result);
+    }
+
+    #[test]
+    fn example2() {
+        let result = part2(EXAMPLE);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn answer2() {
+        let input = fs::read_to_string("./src/day2/input").expect("read input");
+        let result = part2(&input);
+        assert!(result < 327);
+        assert!(result > 261);
+        assert_ne!(result, 265);
+        assert_ne!(result, 266);
+        print!("answer2 {}", result);
     }
 }
