96cebe96a588c5376f720eea35db76c5b0753ecc
Julio Gutierrez <test@test.com>
2024-12-12 16:53:10 -0600

    refactor

--- adventofcode/2024/src/day6/mod.rs
+++ adventofcode/2024/src/day6/mod.rs
@@ -1,18 +1,26 @@
-#[derive(Debug, Copy, Clone)]
+use std::collections::HashSet;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 struct Point {
     x: usize,
     y: usize,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Guard {
+    point: Point,
+    dir: Direction,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Cell {
     Empty,
     Obstruction,
     Guard(Direction),
-    Visited,
+    Visited(HashSet<Direction>),
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -24,41 +32,52 @@ enum Direction {
 fn part1(input: &str) -> usize {
     let mut matrix = get_matrix(input);
 
-    let gp = guard_point(&matrix);
-    if gp.is_none() {
+    let g = get_guard(&matrix);
+    if g.is_none() {
         return 0;
     }
-
-    let mut gp = gp.unwrap();
-    let mut np = next_point(&matrix, gp);
-
-    while let Some(npv) = np {
-        match matrix[npv.y][npv.x] {
-            Cell::Obstruction => {
-                if let Cell::Guard(dir) = matrix[gp.y][gp.x] {
-                    matrix[gp.y][gp.x] = match dir {
-                        Direction::Up => Cell::Guard(Direction::Right),
-                        Direction::Right => Cell::Guard(Direction::Down),
-                        Direction::Down => Cell::Guard(Direction::Left),
-                        Direction::Left => Cell::Guard(Direction::Up),
-                    }
-                }
-            }
-            _ => {
-                matrix[npv.y][npv.x] = matrix[gp.y][gp.x];
-                matrix[gp.y][gp.x] = Cell::Visited;
-                gp = npv;
-            }
-        }
-        np = next_point(&matrix, gp);
-    }
-    matrix[gp.y][gp.x] = Cell::Visited;
+    let guard = g.unwrap();
+    walk(&mut matrix, guard);
 
     matrix
         .iter()
         .flat_map(|row| row.iter())
-        .filter(|cell| matches!(*cell, Cell::Visited))
+        .filter(|cell| matches!(*cell, Cell::Visited(_)))
         .count()
+}
+
+fn walk(matrix: &mut [Vec<Cell>], guard: Guard) {
+    let mut gp = guard.point;
+    let mut gd = guard.dir;
+    let mut np = next_point(matrix, gp, gd);
+
+    while let Some(npv) = np {
+        match matrix[npv.y][npv.x] {
+            Cell::Obstruction => {
+                gd = match gd {
+                    Direction::Up => Direction::Right,
+                    Direction::Right => Direction::Down,
+                    Direction::Down => Direction::Left,
+                    Direction::Left => Direction::Up,
+                }
+            }
+            _ => {
+                visit_cell(&mut matrix[gp.y][gp.x], gd);
+                gp = npv;
+            }
+        }
+        np = next_point(matrix, gp, gd);
+    }
+    visit_cell(&mut matrix[gp.y][gp.x], gd);
+}
+
+fn visit_cell(cell: &mut Cell, dir: Direction) {
+    match cell {
+        Cell::Visited(v) => {
+            v.insert(dir);
+        }
+        _ => *cell = Cell::Visited(HashSet::from([dir])),
+    }
 }
 
 fn get_matrix(input: &str) -> Vec<Vec<Cell>> {
@@ -81,41 +100,41 @@ fn get_matrix(input: &str) -> Vec<Vec<Cell>> {
         .collect::<Vec<Vec<Cell>>>()
 }
 
-fn next_point(matrix: &[Vec<Cell>], point: Point) -> Option<Point> {
-    let mut new = point;
-    if let Cell::Guard(dir) = &matrix[point.y][point.x] {
-        match *dir {
-            Direction::Up => match new.y.checked_sub(1) {
-                Some(y) => new.y = y,
-                None => return None,
-            },
-            Direction::Right => match new.x.checked_add(1) {
-                Some(x) => new.x = x,
-                None => return None,
-            },
-            Direction::Down => match new.y.checked_add(1) {
-                Some(y) => new.y = y,
-                None => return None,
-            },
-            Direction::Left => match new.x.checked_sub(1) {
-                Some(x) => new.x = x,
-                None => return None,
-            },
+fn next_point(matrix: &[Vec<Cell>], point: Point, dir: Direction) -> Option<Point> {
+    let (new, overflow): (Point, bool) = match dir {
+        Direction::Up => {
+            let (y, of) = point.y.overflowing_sub(1);
+            (Point { x: point.x, y }, of)
         }
-    }
-    if new.y >= matrix.len() || new.x >= matrix[new.y].len() {
+        Direction::Right => {
+            let (x, of) = point.x.overflowing_add(1);
+            (Point { x, y: point.y }, of)
+        }
+        Direction::Down => {
+            let (y, of) = point.y.overflowing_add(1);
+            (Point { x: point.x, y }, of)
+        }
+        Direction::Left => {
+            let (x, of) = point.x.overflowing_sub(1);
+            (Point { x, y: point.y }, of)
+        }
+    };
+    if overflow || new.y >= matrix.len() || new.x >= matrix[new.y].len() {
         None
     } else {
         Some(new)
     }
 }
 
-fn guard_point(matrix: &[Vec<Cell>]) -> Option<Point> {
+fn get_guard(matrix: &[Vec<Cell>]) -> Option<Guard> {
     for (y, row) in matrix.iter().enumerate() {
         for (x, cell) in row.iter().enumerate() {
             match cell {
-                Cell::Guard(_) => {
-                    return Some(Point { x, y });
+                Cell::Guard(dir) => {
+                    return Some(Guard {
+                        point: Point { x, y },
+                        dir: *dir,
+                    })
                 }
                 _ => continue,
             }
@@ -126,6 +145,15 @@ fn guard_point(matrix: &[Vec<Cell>]) -> Option<Point> {
 
 #[allow(dead_code)]
 fn part2(input: &str) -> usize {
+    let mut matrix = get_matrix(input);
+
+    let gp = get_guard(&matrix);
+    if gp.is_none() {
+        return 0;
+    }
+    let guard_point = gp.unwrap();
+    walk(&mut matrix, guard_point);
+
     0
 }
 
@@ -164,7 +192,7 @@ mod test {
     #[test]
     fn example2() {
         let result = part2(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, 6);
     }
 
     #[test]
